8d6bfb49e7fa104ecd2d582f9f7601fb31f6195c
Kwesikendy <dogbeynathan7@gmail.com>
2026-06-14 02:49:03 +0000

    fix: aggressively unregister old PWA service workers so clients receive the latest deployed version

--- artifacts/pharmacy/src/main.tsx
+++ artifacts/pharmacy/src/main.tsx
@@ -2,4 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Unregister old service workers to ensure users get the latest version
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
