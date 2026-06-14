d7cbbf60e64b23fed05f7a83c4ec864f6045f9db
Kwesikendy <dogbeynathan7@gmail.com>
2026-06-14 02:24:58 +0000

    fix: remove virtual:pwa-register import from main.tsx - was causing Vercel build to fail

--- artifacts/pharmacy/src/main.tsx
+++ artifacts/pharmacy/src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { Toaster } from "@/components/ui/toaster";
-import { AuthProvider } from "./contexts/AuthContext";
-import { registerSW } from "virtual:pwa-register";
-
-registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")!).render(<App />);
